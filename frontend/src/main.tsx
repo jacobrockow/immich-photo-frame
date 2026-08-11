eaf66249b3206d0484662f81c7a4cfb582a3fd5e
jacobrockow <157256757+jacobrockow@users.noreply.github.com>
2026-08-11 19:07:06 -0400

    Return centralized login after logout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,10 @@ import App from "./App";
 import ConnectionRouter from "./ConnectionRouter";
 import "./styles.css";
 
+window.addEventListener("photoframe-auth-changed", () => {
+  window.location.assign("/");
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ConnectionRouter>
