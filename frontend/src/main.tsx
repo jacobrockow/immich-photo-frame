042386e9bb2491f5aea4e3cd721c154a82875662
jacobrockow <157256757+jacobrockow@users.noreply.github.com>
2026-08-11 19:05:02 -0400

    Route account connection through server settings

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import ConnectionRouter from "./ConnectionRouter";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ConnectionRouter>
+      <App />
+    </ConnectionRouter>
   </React.StrictMode>,
 );
